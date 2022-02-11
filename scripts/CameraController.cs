bc2314707610fd2e7ab78f6a849c912890fdb8bb
Furkan Doğru <frkndgr35@hotmail.com>
2022-02-11 19:29:34 +0300

    Added camera controller codes

--- scripts/CameraController.cs
+++ scripts/CameraController.cs
@@ -6,15 +6,25 @@ public class CameraController : MonoBehaviour
 {
 
     public Transform target;
-    // Start is called before the first frame update
+    public float smoothing;
+    
     void Start()
     {
         
     }
 
-    // Update is called once per frame
+    
     void Update()
     {
-        transform.position = new Vector3(target.transform.position.x, target.transform.position.y, transform.position.z);
+        //transform.position = new Vector3(target.transform.position.x, target.transform.position.y, transform.position.z);
+
+
+        if (transform.position != target.position)
+        {
+            Vector3 targetPosition = new Vector3(target.position.x, target.position.y, transform.position.z);
+
+            transform.position = Vector3.Lerp(transform.position, targetPosition, smoothing);
+        }
+    
     }
 }
