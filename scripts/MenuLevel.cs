02fb8f6b02a8266db81cd2e301c6c210be910226
Furkan Doğru <frkndgr35@hotmail.com>
2022-03-14 13:57:52 +0300

    Added menu level codes

---
+++ scripts/MenuLevel.cs
@@ -0,0 +1,17 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class MenuLevel : MonoBehaviour
+{
+    public void PlayGame()
+    {
+        SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex + 1);
+    }
+
+    public void QuitGame()
+    {
+        Application.Quit();
+    }
+}
